df9826f54704f3d779bab16edd33e934a73e8d52
翔貴 宮川 <s_mygw72@toki.waseda.jp>
2018-09-13 13:51:52 +0900

    18.09.13時点での状態

---
+++ Scripts/Original/MovePrivateSoundsPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePrivateSoundsPosition : MonoBehaviour {
+
+    GameObject publicSound;
+
+	// Use this for initialization
+	void Start () {
+        publicSound = GameObject.Find("PublicSound");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = publicSound.transform.position;
+	}
+}
